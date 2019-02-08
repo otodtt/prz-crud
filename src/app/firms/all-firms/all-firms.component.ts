bcd64bba3f4ff1e822ef7490edc23298ecdac358
otodtt <dtenevoto@mail.bg>
2019-02-08 17:17:48 +0200

    08.02.2019 s2 test firebase

--- src/app/firms/all-firms/all-firms.component.ts
+++ src/app/firms/all-firms/all-firms.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-all-firms',
   templateUrl: './all-firms.component.html',
-  styleUrls: ['./all-firms.component.css']
+  styleUrls: ['./all-firms.component.scss']
 })
 export class AllFirmsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private db: AngularFirestore) { }
 
   ngOnInit() {
+    this.db.collection('firms')
+      .snapshotChanges()
+      .pipe(
+        map(docArray => {
+          console.log(docArray);
+          return docArray.map(doc => {
+            return {
+              id: doc.payload.doc.id,
+              ...doc.payload.doc.data()
+            };
+          });
+        })
+      )
+      .subscribe( result => {
+        console.log(result);
+        // for ( const res of result ) {
+        //   console.log(res.payload.doc.data());
+        // }
+    });
+
+
   }
 
 }
