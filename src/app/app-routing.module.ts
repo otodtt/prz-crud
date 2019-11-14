a3da890182bb59e40075ee280d74c24b2993485f
otodtt <dtenevoto@mail.bg>
2019-11-14 17:13:10 +0200

    14.11.2019 auth test 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule} from '@angular/router';
 
+// Required components for which route services to be activated
+import { SignInComponent } from './auth/components/sign-in/sign-in.component';
+import { SignUpComponent } from './auth/components/sign-up/sign-up.component';
+import { DashboardComponent } from './auth/components/dashboard/dashboard.component';
+import { ForgotPasswordComponent } from './auth/components/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './auth/components/verify-email/verify-email.component';
+
+// Import canActivate guard services
+import { AuthGuard } from './auth/shared/guard/auth.guard';
+import { SecureInnerPagesGuard } from './auth/shared/guard/secure-inner-pages.guard';
+
 import { HomeComponent } from './home/home.component';
 import { NewUserComponent } from './new-user/new-user.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
 import { EditUserResolver } from './edit-user/edit-user.resolver';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', redirectTo: '/sign-in', pathMatch: 'full'},
+  { path: 'sign-in', component: SignInComponent, canActivate: [SecureInnerPagesGuard]},
+  { path: 'register-user', component: SignUpComponent, canActivate: [SecureInnerPagesGuard]},
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'forgot-password', component: ForgotPasswordComponent, canActivate: [SecureInnerPagesGuard] },
+  { path: 'verify-email-address', component: VerifyEmailComponent, canActivate: [SecureInnerPagesGuard] },
+  // { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'new-user', component: NewUserComponent },
   { path: 'details/:id', component: EditUserComponent, resolve: { data : EditUserResolver} },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { HeaderComponent } from './home/header/header.component';
 
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule, FirestoreSettingsToken } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from '../environments/environment';
 
 import { FirebaseService } from './services/firebase.service';
@@ -24,7 +25,26 @@ import { FirstLetterService } from './services/first.letter.service';
 import { HttpErrorHandler } from './services/http-error-handler.service';
 import { MessageService } from './services/message.service';
 
+// AUTH components
+import { SignInComponent } from './auth/components/sign-in/sign-in.component';
+import { SignUpComponent } from './auth/components/sign-up/sign-up.component';
+import { DashboardComponent } from './auth/components/dashboard/dashboard.component';
+import { ForgotPasswordComponent } from './auth/components/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './auth/components/verify-email/verify-email.component';
+
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+
+// Firebase services + enviorment module
+// import { AngularFireModule } from '@angular/fire';
+// import { AngularFireAuthModule } from '@angular/fire/auth';
+// import { AngularFirestoreModule } from '@angular/fire/firestore';
+// import { environment } from '../environments/environment';
+
+// Auth service
+import { AuthService } from './auth/shared/services/auth.service';
+
+
 import {
   MatButtonModule,
   MatInputModule,
@@ -33,6 +53,7 @@ import {
   MatIconModule,
   MatListModule,
 } from '@angular/material';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -42,6 +63,11 @@ import {
     HomeComponent,
     SideNavComponent,
     HeaderComponent,
+    SignInComponent,
+    SignUpComponent,
+    DashboardComponent,
+    ForgotPasswordComponent,
+    VerifyEmailComponent
   ],
   entryComponents: [AvatarDialogComponent],
   imports: [
@@ -50,6 +76,7 @@ import {
     ReactiveFormsModule,
     AppRoutingModul,
     AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
     AngularFirestoreModule,
     BrowserAnimationsModule,
     MatButtonModule,
@@ -65,6 +92,7 @@ import {
     FirstLetterService,
     HttpErrorHandler,
     MessageService,
+    AuthService,
     { provide: FirestoreSettingsToken, useValue: {} },
     EditUserResolver],
   bootstrap: [AppComponent],
